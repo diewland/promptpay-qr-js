b4fbfc045d20b234eaf43496cc7e14f524d5f68a
Yuwat Chuesathuchon <diewland@gmail.com>
2017-10-09 11:55:28 +0700

    change qr-type from one-time to never-expire

--- promptpay-qr.js
+++ promptpay-qr.js
@@ -90,7 +90,7 @@ var PromptPayQR = {
       pp_amount = '54' + ('00' + amount.length).slice(-2) + amount;
     }
     // build pp string
-    var pp_str = "00020101021229370016A000000677010111"
+    var pp_str = "00020101021129370016A000000677010111"
                   + pp_acc_id
                   + "5303764"
                   + pp_amount
